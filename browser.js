a399a7b2154bc063cd4eaf79f9735c7fdcc6300d
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-04-02 14:45:43 +0800

    update

--- browser.js
+++
@@ -1,8 +0,0 @@
-const inBrowser = typeof window !== "undefined";
-const UA = inBrowser && window.navigator.userAgent.toLowerCase();
-const isIE = UA && /msie|trident/.test(UA);
-
-if (isIE) {
-  document.getElementById("top-loader").style.display = "none";
-  document.getElementById("IEBrowser-tips").style.display = "block";
-}
