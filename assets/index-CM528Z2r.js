fb07222a218a12725adfcf2a1361e95715c8aa77
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-04-02 14:48:56 +0800

    update

---
+++ assets/index-CM528Z2r.js
@@ -0,0 +1,21 @@
+import{_ as a,o as t,b as n,d as e,r,R as h,h as i,g as c,F as p,D as s}from"./index-Dh65PKXK.js";const u={},m={class:"text-sm"},f=e("p",{class:"font-semibold"},"我是子组件",-1),v=e("p",null,"page2",-1),x=[f,v];function g(_,l){return t(),n("div",m,x)}const b=a(u,[["render",g]]),k={class:"flex"},w={class:"w-80 h-80 rounded-2xl relative text-center shadow"},C=e("p",{class:"font-semibold"},"我是父组件",-1),R=e("p",null,"page1",-1),$={class:"flex mt-8"},j={class:"w-80"},B=e("p",{class:"font-bold"},"父组件代码：",-1),D=e("pre",null,[s("        "),e("code",null,`
+<div>
+  我是父组件
+  <Child/>
+</div>
+
+<script setup>
+  import Child from "./Child.vue";
+<\/script>
+        `),s(`
+    `)],-1),N=[B,D],V={class:"ml-4 w-80"},y=e("p",{class:"font-bold"},"子组件代码：",-1),F=e("pre",null,[s("        "),e("code",null,`
+<template>
+  <div>我是子组件</div>
+</template>
+        `),s(`
+    `)],-1),L=[y,F],E={class:"ml-4"},T=e("p",{class:"font-bold"},"目录结构：",-1),q=e("pre",null,[s("        "),e("code",{class:"html"},`
+|-- folder                // 文件夹
+      |-- index.vue       // 父组件
+      |-- Child.vue       // 子组件
+        `),s(`
+    `)],-1),z=[T,q],I={__name:"index",setup(_){const l=[{label:"vue3 setup 模式中如何使用组件？",link:"https://cn.vuejs.org/api/sfc-script-setup.html#using-components"},{label:"vue3 setup 模式中如何给引用的组件重新命名？",link:"https://cn.vuejs.org/api/sfc-script-setup.html#recursive-components"}];return(A,G)=>{const d=r("RhReferenceLink"),o=h("highlight");return t(),n(p,null,[e("div",k,[e("div",w,[C,R,i(b,{class:"w-40 h-40 text-center shadow-md flex flex-col items-center justify-center absolute transform -translate-x-2/4 -translate-y-1/2 top-2/4 left-2/4"})])]),e("div",$,[c((t(),n("div",j,N)),[[o]]),c((t(),n("div",V,L)),[[o]]),c((t(),n("div",E,z)),[[o]])]),i(d,{links:l})],64)}}};export{I as default};
---
+++ assets/index-DvPIXyz2.js
@@ -0,0 +1,8 @@
+import{r as n,R as d,o as t,b as a,d as e,D as s,h as l,w as r,g as h,X as u}from"./index-Dh65PKXK.js";const _={class:"flex items-baseline"},m=e("p",{class:"mt-3"}," 二、下载完成后，使用命令行工具进入 v3s 的目录，然后按顺序执行以下命令。（需要保证你的 Node 版本在 20.10.0 及以上。） ",-1),v={class:"mt-3 w-[300px]"},f=e("pre",null,[s("        "),e("code",null,`
+// 安装依赖
+npm install
+
+// 启动项目
+npm run dev
+        `),s(`
+      `)],-1),g=[f],k=u('<div><p>三、在浏览器中，有两种启动方式：</p><p class="mt-1 indent-8"> 1、在浏览器地址栏，输入： <span class="underline underline-offset-4">http://localhost:5173/v3s-admin/</span> ，然后按回车。 </p><p class="mt-1 indent-8"> 2、执行完 npm run dev 后，停留在你的命令行工具中，输入字母 o ，然后回车，浏览器会自动打开 <span class="underline underline-offset-4">http://localhost:5173/v3s-admin/</span> 。 </p></div><p class="mt-3"> 四、使用 vsCode 打开 v3s 目录。使用组合按键 <span class="underline underline-offset-4">Ctrl + Shift + X</span> 唤醒 “扩展”，在 “扩展” 中搜索 <span class="underline underline-offset-4">V3S Snippets</span> ，选中，在左侧工作区弹出的窗口中，点击 <span class="underline underline-offset-4">安装</span> 。 </p><p class="mt-3">五、至此，关于 v3s 的安装和启动工作就都完成了。</p>',3),C={__name:"index",setup(b){const i=[{label:"如何搭建前端开发环境？如何安装 git、nodeJS、vsCode？",link:"https://github.com/wangyupo/FE-Guide"},{label:"v3s的项目地址在哪儿？",link:"https://github.com/wangyupo/v3s"}];return(w,x)=>{const o=n("el-link"),c=n("RhReferenceLink"),p=d("highlight");return t(),a("div",null,[e("p",_,[s(" 一、使用 git 下载 v3s，这是下载地址： "),l(o,{type:"primary",href:"https://github.com/wangyupo/v3s",target:"_blank",class:"text-lg"},{default:r(()=>[s(" https://github.com/wangyupo/v3s ")]),_:1})]),m,h((t(),a("div",v,g)),[[p]]),k,l(c,{class:"mt-16",links:i})])}}};export{C as default};
